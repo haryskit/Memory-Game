fd6cc35b46838325f2b106028f069432d45e7fbd
Harshit Singh Chouhan <harshitdev292@gmail.com>
2025-12-11 14:30:35 +0530

    update many thigns compelte deploy

--- react-memory-game/capacitor.config.ts
+++ react-memory-game/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.memorygame.app',
+  appId: 'app.memorymastergo.com',
   appName: 'Memory Master GO',
   webDir: 'build'
 };
